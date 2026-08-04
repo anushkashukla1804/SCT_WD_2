82c2e19b7cc5bccf9521274641eb05f693ee7a4d
anushkashukla1804 <anu.936970@gmail.com>
2026-08-04 20:40:10 +0530

    work done

---
+++ script.js
@@ -0,0 +1,46 @@
+ const display = document.getElementById("display");
+
+function append(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
+
+// Keyboard Input Handling
+document.addEventListener("keydown", function(event){
+
+    const key = event.key;
+
+    if(
+        "0123456789+-*/.".includes(key)
+    ){
+        append(key);
+    }
+
+    else if(key === "Enter"){
+        calculate();
+    }
+
+    else if(key === "Backspace"){
+        deleteLast();
+    }
+
+    else if(key === "Escape"){
+        clearDisplay();
+    }
+});
